43f80ee5ed39f08c4119f95e0a8e0bd269d4a0ab
abubakar.waqas <abubakar.waqas@invozone.com>
2021-08-01 14:58:09 +0500

    update dist packages

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-var apiUrl = process.env.NODE_ENV === 'development'
-    ? process.env.NEXT_PUBLIC_WEBSITE_URL_DEVELOPMENT // development api
-    : process.env.NEXT_PUBLIC_WEBSITE_URL_PRODUCTION; // prodSuction api
-export { apiUrl };
-// Purpose during deployment
--- pages/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function Home() {
-    return (React.createElement("div", { className: styles.container },
-        React.createElement("h1", null, "Hello World")));
-}
-exports["default"] = Home;
